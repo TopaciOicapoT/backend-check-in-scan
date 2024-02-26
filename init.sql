2f9d7e0decd4ae041bcca64928c37c3658d8a61a
TopaciOicapoT <115380138+TopaciOicapoT@users.noreply.github.com>
2024-02-26 16:25:19 +0100

    modifica init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
--- Crea tabla productos al iniciar
+-- Crea tabla productos al iniciar, la variable estatus se usa coo true o false, siendo 0 false y 1 true
 CREATE TABLE my_database.products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
